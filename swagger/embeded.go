ed858edd80e2f581c3557988edc732e4a41d5314
NekNArej <nikita.58baydin4@mail.ru>
2026-04-30 19:43:46 +0300

    swagger go

--- swagger/embeded.go
+++ swagger/embeded.go
@@ -4,6 +4,6 @@ import "embed"
 
 // Внедряем папки со спецификациями
 //
-//go:embed article-service/*
-//go:embed user-service/*
+//go:embed docs/article-service/*
+//go:embed docs/user-service/*
 var SpecsFS embed.FS
