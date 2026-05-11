a3614edd2cbdcb36b936bad47697cc8b4401f42b
NekNArej <nikita.58baydin4@mail.ru>
2026-05-11 16:07:57 +0300

    Update swagger-initializer.js

--- backend/gateway-server/internal/assets/swagger/swagger-initializer.js
+++ backend/gateway-server/internal/assets/swagger/swagger-initializer.js
@@ -4,15 +4,10 @@ window.onload = function () {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-      {
-        name: "User Service",
-        url: "/specs/docs/user/user.swagger.json", // Ссылка должна совпадать с тем, что в Go коде
-      },
-
       // TODO: Добавить новые пути
       {
         name: "Article Service",
-        url: "/specs/docs/article-service/article.swagger.json",
+        url: "/specs/docs/article-service/article.swagger.yaml",
       },
     ],
     dom_id: "#swagger-ui",
