d12adbdfb5d1c6bc9f95d9bb096b8e761de8f5dd
Gloryllew <jumper007agent@gmail.com>
2026-05-13 14:21:09 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
+import { Route, Routes } from 'react-router'
+import Home from './components/pages/home/Home'
+import Article from './components/pages/article/Article'
+import Simulator from './components/pages/simulator/Simulator'
 
-  return (<></>)}
+function App() {
+    return (
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/article/:id' element={<Article/>} />
+            <Route path='/simulator' element={<Simulator/>} />
+            <Route path='*' element={'Not Found'} />
+        </Routes>
+    )
+}
 
 export default App
